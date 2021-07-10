a05209b233d3f22ed37b4d66112bfe359d635d16
Jane Njeri <daktari2033@gmail.com>
2021-07-10 06:17:04 +0300

    completed logic for profile edits

--- client/src/components/EditProfile/EditProfile.js
+++ client/src/components/EditProfile/EditProfile.js
@@ -1,71 +1,142 @@
 import React, { useState } from "react";
 
-export default function EditProfile({ previousUserDetails }) {
-  const { username, bio, twitter, facebook, website } = previousUserDetails;
-  const [updatedUserDetails, setUpdatedDetails] = useState([]);
+import "./EditProfile.css";
 
-  function handleInputeChange(e, field) {
-    if (e.target.value !== field && e.target.value.trim() !== "") {
-      setUpdatedDetails([
-        ...updatedDetails,
-        { field: e.target.value, updated: true },
+import { useAuth } from "../../contexts/AuthContext";
+import { database } from "../../firebase";
+
+export default function EditProfile({
+  userDetails,
+  setEditing,
+  setLoading,
+  setUserDetails,
+}) {
+  const { currentUser } = useAuth();
+  const { username, bio, twitter, facebook, website } = userDetails;
+
+  const [updatedUserDetails, setUpdatedUserDetails] = useState([]);
+
+  function handleInputeChange(e, data, field) {
+    if (!data || (e.target.value !== data && e.target.value.trim() !== "")) {
+      setUpdatedUserDetails([
+        ...updatedUserDetails,
+        { [field]: e.target.value, updated: true },
       ]);
     } else {
-      setUpdatedDetails([...updatedDetails, { field, updated: false }]);
+      setUpdatedUserDetails([
+        ...updatedUserDetails,
+        { [field]: data, updated: false },
+      ]);
     }
   }
 
   async function updateUserDetails() {
-    const newDetails = {};
-
-    updatedUserDetails.forEach((updatedUserDetail) => {
-      if (updatedUserDetail.updated) {
-        for (const key in updatedUserDetail) {
-          if (key !== "updated" && updatedUserDetail.hasOwnProperty(key)) {
-            newDetails.key = updatedUserDetail[key];
+    try {
+      setLoading(true);
+      let newDetails = {};
+      updatedUserDetails.forEach((updatedUserDetail) => {
+        if (updatedUserDetail.updated) {
+          for (const key in updatedUserDetail) {
+            if (key !== "updated" && updatedUserDetail.hasOwnProperty(key)) {
+              newDetails[key] = updatedUserDetail[key];
+            }
           }
         }
+      });
+
+      if (!Object.keys(newDetails) < 1) {
+        const keys = Object.keys(newDetails).map((key) => {
+          return key;
+        });
+        let trimUserDetails = userDetails;
+        keys.forEach((key) => {
+          delete trimUserDetails[key];
+        });
+        setUserDetails({ ...trimUserDetails, ...newDetails });
+        console.log({ ...trimUserDetails, ...newDetails });
+        await database.users.doc(currentUser.uid).update(newDetails);
+        setLoading(false);
+        setEditing(false);
+      } else {
+        setLoading(false);
+        setEditing(false);
       }
-    });
+    } catch (err) {
+      setLoading(false);
+      console.log(err);
+    }
+  }
+
+  function cancelUpdate() {
+    setEditing(false);
   }
 
   return (
     <div className="editprofile">
       <div className="editprofile__details">
-        <input
-          value={username}
-          onChange={(e) => handleInputeChange(e, username)}
-          type="text"
-          placeholder="Name"
-        />
-        <input
-          value={bio}
-          onChange={(e) => handleInputeChange(e, bio)}
-          type="text"
-          placeholder="Short bio"
-        />
-        <input
-          value={twitter}
-          onChange={(e) => handleInputeChange(e, twitter)}
-          type="text"
-          placeholder="Twitter"
-        />
-        <input
-          value={facebook}
-          onChange={(e) => handleInputeChange(e, facebook)}
-          type="text"
-          placeholder="Facebook"
-        />
-        <input
-          value={website}
-          onChange={(e) => handleInputeChange(e, website)}
-          type="text"
-          placeholder="Website"
-        />
+        <label>
+          User Name
+          <input
+            onChange={(e) => {
+              const field = "username";
+              handleInputeChange(e, username, field);
+            }}
+            type="text"
+            placeholder={username ?? ""}
+          />
+        </label>
+        <label>
+          Bio
+          <input
+            onChange={(e) => {
+              const field = "bio";
+              handleInputeChange(e, bio, field);
+            }}
+            type="text"
+            placeholder={bio ?? ""}
+          />
+        </label>
+        <label>
+          Twitter
+          <input
+            onChange={(e) => {
+              const field = "twitter";
+              handleInputeChange(e, twitter, field);
+            }}
+            type="text"
+            placeholder={twitter ?? ""}
+          />
+        </label>
+        <label>
+          Facebook
+          <input
+            onChange={(e) => {
+              const field = "facebook";
+              handleInputeChange(e, facebook, field);
+            }}
+            type="text"
+            placeholder={facebook ?? ""}
+          />
+        </label>
+        <label>
+          <p>Website</p>
+          <input
+            onChange={(e) => {
+              const field = "website";
+              handleInputeChange(e, website, field);
+            }}
+            type="text"
+            placeholder={website ?? ""}
+          />
+        </label>
       </div>
       <div className="editprofile__buttons">
-        <p onClick={updateUserDetails}>Update</p>
-        <p onClick={cancleUpdate}>Cancel</p>
+        <p className="editprofile__buttons-Update" onClick={updateUserDetails}>
+          Update
+        </p>
+        <p className="editprofile__buttons-Cancel" onClick={cancelUpdate}>
+          Cancel
+        </p>
       </div>
     </div>
   );
--- client/src/components/UserDashboard/UserDashboard.js
+++ client/src/components/UserDashboard/UserDashboard.js
@@ -5,23 +5,30 @@ import "./UserDashboard.css";
 import Navigation from "../Navigation/Navigation";
 import { useAuth } from "../../contexts/AuthContext";
 import { database } from "../../firebase";
+import EditProfile from "../EditProfile/EditProfile";
 
 export default function UserDashboard() {
   const { currentUser } = useAuth();
   const [userDetails, setUserDetails] = useState();
+  const [editing, setEditing] = useState(true);
+  // const [errors, setErrors] = useState({});
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function getUserData() {
       try {
+        setLoading(true);
         const userDoc = await database.users.doc(currentUser.uid).get();
         const user = database.formatDocument(userDoc);
         setUserDetails(user);
+        setLoading(false);
       } catch (err) {
+        setLoading(false);
         console.log(err);
       }
     }
     getUserData();
-  });
+  }, [currentUser.uid]);
 
   if (!userDetails) {
     return (
@@ -36,22 +43,33 @@ export default function UserDashboard() {
       <div className="dashboard__navigation">
         <Navigation />
       </div>
-      <div className="dasboard__content">
-        <div className="dashboard__content-profile">
-          <div className="dashboard__content-profileImage">
-            <img
-              src={userDetails.profilePicture.url}
-              alt={userDetails.username}
-            />
+      <div className="dashboard__content">
+        {editing && (
+          <EditProfile
+            setEditing={setEditing}
+            setLoading={setLoading}
+            setUserDetails={setUserDetails}
+            userDetails={userDetails}
+          />
+        )}
+        {!editing && (
+          <div className="dashboard__content-profile">
+            <div className="dashboard__content-profileImage">
+              <img
+                src={userDetails.profilePicture.url}
+                alt={userDetails.username}
+              />
+            </div>
+            <p>{userDetails.username}</p>
+            <p onClick={() => setEditing(true)}>Update Profile</p>
+            <p>{userDetails.email}</p>
+            {userDetails.bio && <p>{userDetails.bio}</p>}
+            {userDetails.twitter && <p>{userDetails.twitter}</p>}
+            {userDetails.facebook && <p>{userDetails.facebook}</p>}
+            {userDetails.website && <p>{userDetails.website}</p>}
+            <p>{editing ? "Editing" : "Not Editing"}</p>
           </div>
-          <p>{userDetails.username}</p>
-          <p>Update Profile</p>
-          <p>{userDetails.email}</p>
-          {userDetails.bio && <p>{userDetails.bio}</p>}
-          {userDetails.twitter && <p>{userDetails.twitter}</p>}
-          {userDetails.facebook && <p>{userDetails.facebook}</p>}
-          {userDetails.website && <p>{userDetails.website}</p>}
-        </div>
+        )}
         <div className="dashboard__content-articles">
           <p>Articles go Here</p>
         </div>
